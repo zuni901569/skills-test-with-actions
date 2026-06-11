913ea4a0c1b16a440e438746424f161c553043aa
zuni901569 <292620860+zuni901569@users.noreply.github.com>
2026-06-11 01:38:04 +0000

    fix test

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -59,12 +59,12 @@ def test_get_nth_fibonacci_one():
 
 
 def test_get_nth_fibonacci_ten():
-    """Test with n=10."""
-    # Arrange
-    n = 10
+   """Test with n=10."""
+   # Arrange
+   n = 10
 
-    # Act
-    result = get_nth_fibonacci(n)
+   # Act
+   result = get_nth_fibonacci(n)
 
-    # Assert
-    assert result == 89
+   # Assert
+   assert result == 55
