497598c7a82e49938b6bcf3c5a1bcb0026caff23
zuni901569 <292620860+zuni901569@users.noreply.github.com>
2026-06-11 01:39:46 +0000

    100% coverage

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -68,3 +68,21 @@ def test_get_nth_fibonacci_ten():
 
    # Assert
    assert result == 55
+
+def test_area_of_circle_negative_radius():
+   """Test with a negative radius to raise ValueError."""
+   # Arrange
+   radius = -1
+
+   # Act & Assert
+   with pytest.raises(ValueError):
+      area_of_circle(radius)
+
+def test_get_nth_fibonacci_negative():
+   """Test with a negative number to raise ValueError."""
+   # Arrange
+   n = -1
+
+   # Act & Assert
+   with pytest.raises(ValueError):
+      get_nth_fibonacci(n)
